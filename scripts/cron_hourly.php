843ca47236bf66e33b920be07f6f3a4370a156c1
maan81 <pranijman@gmail.com>
2014-10-14 16:54:28 +0545

    crom tmp 2

--- scripts/cron_hourly.php
+++ scripts/cron_hourly.php
@@ -114,6 +114,9 @@ echo PHP_EOL;
 $log_data = new Logging();
 $log_data->lfile($config['log']['dir'].'data.log');
 
+   // number of item
+   $item_count = 0;
+
 
    //grab feed for each symbol & push into db
    foreach ($config['symbols'] as $symbol) {
@@ -143,36 +146,62 @@ $log_data->lfile($config['log']['dir'].'data.log');
 
 
 // $log_data->lwrite(  PHP_EOL.
-//                print_r($item->feed->data['ordered_items'],true).
+               print_r($item->feed->data['items']/**/[0]/**/->data['child']['']);//.
+               print_r($item->feed->data['items']/**/[0]/**/->data['child']['http://www.bing.com:80/news/search?format=rss&q='.$symbol]);//.
+               // print_r($item->feed->data['items']/**/[0]/**/->data['child']['']);//.
 //                PHP_EOL
 //             );
-// die;
+die;
 
 
-         $subdir = ($i%2?'one':'two');
+         $j=-1;
 
-         // extract current feed array
+         // extract current item array & store into db
          foreach($item->feed->data['ordered_items'] as $val){
+            $j++;
+            if($j>=5){break;}
+
+            $item_count++;
+               // randomized img from stored imgs.
+            $subdir = ($item_count%2?'one':'two');
+
             $data = ['symbol'      => $symbol,
-                     'title'       => $val['data']['child']['']['title'][0]['data'],
-                     'url'         => $val['data']['child']['']['link'][0]['data'],
-                     'description' => $val['data']['child']['']['description'][0]['data'],
-                     'datetime'    => $val['data']['date']['raw'],
+                     'title'       => $val->data['child']['']['title'][0]['data'],
+                     'url'         => $val->data['child']['']['link'][0]['data'],
+                     'description' => $val->data['child']['']['description'][0]['data'],
+                     'datetime'    => date("Y-m-d H:i:s",$val->data['date']['parsed']),
                   ];
 
+
             //the url decoded
-            $data['url'] = urldecode( explode('&', explode("?q=",$data['url'])[1] )[0] );  
+            $data['url'] = urldecode(
+                                       explode( "=",
+                                                explode( "&",
+                                                         explode(
+                                                                  "?",
+                                                                  $data['url']
+                                                               )[1]
+                                                      )[3]
+                                             )[1]
+                                    );
+
+
    
             // img of current feed
-            if(isset($val['data']['http://www.bing.com:80/news/search?format=rss&q=aaa']['Image'])){
+            if(isset($val->data['child']
+                                 ['http://www.bing.com:80/news/search?format=rss&q='.$symbol]
+                                 ['Image'])){
                //img from feed
-               $img_url = $val['data']['http://www.bing.com:80/news/search?format=rss&q=aaa']['Image'][0]['data'];
+               $img_url = $val->data['child']
+                                 ['http://www.bing.com:80/news/search?format=rss&q='.$symbol]
+                                 ['Image'][0]['data'];
+
                //corrected img url
                $img_url = urldecode( explode('&', explode("?q=",$img_url)[1] )[0] ); 
             
 
                //---------------------
-               //get & store external img.
+               //download & store external img. into our server
 
                   // path of img to store
                   $img_path = dirname(dirname(__FILE__)).'/img/newsimg/'.$subdir.'/';
@@ -182,10 +211,18 @@ $log_data->lfile($config['log']['dir'].'data.log');
                                        new FilesystemIterator( $img_path, 
                                                                FilesystemIterator::SKIP_DOTS
                                                             )
-                                       ) ;
+                                       )+1;
+
 
                   // file extension
-                  $ext = $config['extensions'][exif_imagetype($img_url)];
+                  $ext = @$config['extensions'][exif_imagetype($img_url)];
+                  if( ! $ext ){
+                                    // $rand_img_id is from lines 102~111
+                     $img_localurl='static/'.$files[$rand_img_id];
+                     $rand_img_id++;
+                     $rand_img_id =  ($rand_img_id > count($files))? 0 : $rand_img_id;
+
+                  }else{
 
 
                   $img = $img_path.$num_of_files.'.'.$ext;
@@ -202,104 +239,28 @@ $log_data->lfile($config['log']['dir'].'data.log');
                   $image->resize(480,320); 
                   $image->save($img);
 
+               }
+
                //---------------------
 
             }else{
-               // randomized img from stored imgs.
-               $data['image'] = '';
 
                // $rand_img_id is from lines 102~111
                $img_localurl='static/'.$files[$rand_img_id];
                $rand_img_id++;
+               $rand_img_id =  ($rand_img_id > count($files))? 0 : $rand_img_id;
 
             }
             // set the img url to our data
             $data['image'] = $img_localurl;
 
-            _print_r($data);
+            print_r($data);
+            // die;
 
-
-
-
-
-//          // img of current feed
-//          if(isset($x['Image'])){
-
-//             $x = $x['Image'][0]['data'];
-
-//             // img url from the feed
-//             $img_url = urldecode( explode('&', explode("?q=",$x)[1] )[0] ); 
-
-//             // path of img to store
-//             $img_path = dirname(dirname(__FILE__)).'/img/newsimg/'.$subdir.'/';
-
-//             // rename img
-//             $num_of_files = iterator_count(
-//                                  new FilesystemIterator( $img_path, 
-//                                                          FilesystemIterator::SKIP_DOTS
-//                                                       )
-//                                  ) ;
-
-//             // file extension
-//             $ext = $config['extensions'][exif_imagetype($img_url)];
-
-//             $img = $img_path.$num_of_files.'.'.$ext;
-
-//             // set url of img
-//             $img_localurl = $subdir.'/'.$num_of_files.'.'.$ext;
-
-
-//             // store img from external url to local server
-//             copy($img_url, $img);
-            
-// echo PHP_EOL;
-// print_r($img_url);
-// echo PHP_EOL;
-// print_r($img);
-
-//             // resize img to 480x320 
-//             $image = new SimpleImage(); 
-//             $image->load($img); 
-//             $image->resize(480,320); 
-//             $image->save($img);
-
-//          // no img present in feed - use rand. imgs from seperate directory
-//          }else{
-            
-
-// echo PHP_EOL;
-// print_r('---');
-// echo PHP_EOL;
-// print_r('---');
-//             // $img_url='';
-//             $img_localurl='static/'.$files[$rand_img_id];
-//             $rand_img_id++;
-//          }
-
-
-//          // init data to store
-//          $data = ['symbol'      => $symbol,
-//                   'title'       => $item->get_title(),
-//                   'description' => $item->get_description(),
-//                   'url'         => urldecode(
-//                                           explode( "=",
-//                                                    explode( "&",
-//                                                             $item->get_link()
-//                                                       ) [3]
-//                                                 ) [1]
-//                                        ),
-//                   'datetime'    => $item->get_date('Y-m-d'),
-//                   'image'       => $img_localurl,
-//                ]; 
-// echo PHP_EOL;
-// print_r($data);
-// echo PHP_EOL;
-
-//          // store in db
-//          $db->insert($config['db']['table_news'],$data);
-
+            // store in db
+            $db->insert($config['db']['table_news'],$data);
+         }
       }
-
    }
 
 //==============================================================================
