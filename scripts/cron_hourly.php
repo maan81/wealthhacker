f009b2e85b98972f086aaa6b756d802e37549f0a
maan81 <pranijman@gmail.com>
2014-10-20 12:22:27 +0545

    cron_hourly cleanup

--- scripts/cron_hourly.php
+++ scripts/cron_hourly.php
@@ -58,33 +58,6 @@
 
    $data = $db->get($config['db']['table_news'], $where_clause);
 
-   /*
-      // // get the ids of the rows to be deleted
-      // $ids = [];
-      // foreach($data as $key=>$val){
-      //    $ids[] = $val['id'];
-      // }
-
-
-      // // generate where clause from ids
-      // $where_clause = ' AND id IN ('.implode(',', $ids).')';
-
-      // // delete old rows having the id
-      // $db->del($config['db']['table_news'], $where_clause);
-
-      // //delete old imgs of the ids
-      // foreach($data as $key=>$val){
-
-      //    //but dont delete the no-image
-      //    if(strpos($val['image'],'static') !== false) {
-      //       continue;
-      //    }
-
-      //    unlink($src.'/img/newsimg/'.$val['image']);
-
-      // }
-   */
-
    // delete old rows having the id
    $db->del($config['db']['table_news'], $where_clause);
 
@@ -99,6 +72,9 @@
 //==============================================================================
 // Get new feed & update db & imgs.
 
+   // init array to hold inserted news for log
+   $log_dbchange_data = [];
+
    //init feed
    $feed = new SimplePie();
 
@@ -110,18 +86,6 @@
 
    //provide the caching folder
    $feed->set_cache_location('cache');
-
-   // //-----------------------------------
-   // // prepare to randomize the existing imgs .... 
-   // // for news which which dones not contain its own img.
-   //    $dir    = 'img/newsimg/static/';
-   //    $files = scandir($src.'/'.$dir);
-   //    unset($files[0]);
-   //    unset($files[1]);
-   //    shuffle($files);
-   //    $files = array_values($files);
-   //    $rand_img_id = 0;
-   // //-----------------------------------
 
    // number of item
    $item_count = 0;
@@ -183,94 +147,14 @@
          $tmp = $db->get($config['db']['table_news'],' AND url="'.$data['url'].'" ');
          if(count($tmp)) continue;
 
-
-         /*
-            // // img of current feed
-            // if(isset($val['child']['http://www.bing.com:80/news/search?format=rss&q='.$symbol]['Image'])){
-
-            //    // get img from feed
-            //    $img_url = $val['child']
-            //                   ['http://www.bing.com:80/news/search?format=rss&q='.$symbol]
-            //                   ['Image'][0]['data'];
-               
-            //    //corrected img url
-            //    $img_url = urldecode( explode('&', explode("?q=",$img_url)[1] )[0] ); 
-            
-
-            //    //---------------------
-            //    //download & store img. from external server into our server ...
-
-            //       // path of img to store
-            //       $img_path = dirname(dirname(__FILE__)).'/img/newsimg/'.$subdir.'/';
-
-            //       // get unique integer to set filename
-            //       while(true){
-            //          $item_count++;
-            //          $g = glob ($img_path.'/'.$item_count.'.*');
-            //          if(empty($g))
-            //             break;
-            //       }
-
-
-            //       // file extension
-            //       $ext = @$config['extensions'][exif_imagetype($img_url)];
-                  
-            //       // if external img err, use our server's imgs.
-            //       if( ! $ext ){
-
-            //          // select a particular rand. img.
-            //          // $rand_img_id is from lines 97~105
-
-            //          $img_localurl='static/'.$files[$rand_img_id];
-            //          $rand_img_id++;
-            //          $rand_img_id =  ($rand_img_id > count($files))? 0 : $rand_img_id;
-
-            //       }else{
-
-            //          // rename the downloaded img to integer
-            //          $img = $img_path.$item_count.'.'.$ext;
-
-            //          // set url of img
-            //          $img_localurl = $subdir.'/'.$item_count.'.'.$ext;
-
-            //          // store img from external url to local server
-            //          copy($img_url, $img);
-
-            //          // resize img to 480x320 
-            //          $image = new SimpleImage(); 
-            //          $image->load($img); 
-            //          $image->resize(480,320); 
-            //          $image->save($img);
-
-            //       }
-
-            //    //---------------------
-
-            // }else{
-
-            //    // select a particular rand. img.
-            //    // $rand_img_id is from lines 97~105
-
-            //    $img_localurl='static/'.$files[$rand_img_id];
-            //    $rand_img_id++;
-            //    $rand_img_id =  ($rand_img_id >= count($files))? 0 : $rand_img_id;
-
-            // }
-            // // set the img url to our data
-            // $data['image'] = $img_localurl;
-
-            $log_dbchange->lwrite(print_r('Stored Img :',true));
-            $log_dbchange->lwrite(print_r($data,true));
-            // die;
-         */
-         
          // store in db
          $db->insert($config['db']['table_news'],$data);
 
-         $log_dbchange->lwrite(print_r('Stored Img :',true));
-         $log_dbchange->lwrite(print_r($data,true));
+         $log_dbchange_data[] = $data;
       }
    }
+   $log_dbchange->lwrite(print_r('Stored Data :',true));
+   $log_dbchange->lwrite(print_r($log_dbchange_data,true));
 //==============================================================================
 
 
